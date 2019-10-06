6e967e864c7e84afe08f1e466ec34a80c17ffd70
aungwin <aungthiwin@sys-city.com>
2019-10-06 22:23:54 +0630

    adding chat

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import VueChatScroll from 'vue-chat-scroll'
 
 Vue.config.productionTip = false
+Vue.use(VueChatScroll)
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Chat from './components/Chat.vue'
+import Room from './components/Room.vue'
+import AddRoom from './components/AddRoom.vue'
+import Login from './components/Login.vue'
 
 Vue.use(Router)
 
